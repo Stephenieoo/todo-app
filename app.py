1642e6dc58f04e33e51fb0a688eae36f33484074
Stephenieoo <xo258@nyu.edu>
2025-10-29 23:39:54 -0400

    Update Flask app configuration and dependencies
    - Add app.py health and ready route
    - Update the version of Werkzeug in requirements.txt
    - Cleaned .DS_Store artifacts

--- app.py
+++ app.py
@@ -119,6 +119,14 @@ def search():
 def about():
 	return render_template('credits.html',t=title,h=heading)
 
+@app.route("/health")
+def health():
+	return "OK", 200
+
+@app.route("/ready")
+def ready():
+	return "OK", 200
+
 if __name__ == "__main__":
 	env = os.environ.get('FLASK_ENV', 'development')
 	port = int(os.environ.get('PORT', 5000))
